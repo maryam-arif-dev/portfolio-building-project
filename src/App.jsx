8eeac9119a93937da55951a9735d25e1edbb602b
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-01-08 15:44:53 +0430

    chore: initialize generalInfo configuration object
     Add a structured object to hold general portfolio information, including contact details and technical skill lists, for easier data management.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import "./App.css";
 import PortfolioPage from "./components/PortfolioPage";
 function App() {
-  // in here i have to create a object for fatching data
+  // Define the generalInfo Object for fatching data
+  const generalInfo = {
+    name: "Maryam Arif",
+    logo: "",
+    headline:
+      "Building performant, accessible web experiences that drive user engagement",
+    subHeadline: "",
+    aboutMe: "",
+    skils: [
+      { name: "HTML", icon: "" },
+      { name: "CSS", icon: "" },
+      { name: "Bootstrap", icon: "" },
+      { name: "JavaScript", icon: "" },
+      { name: "jQuery", icon: "" },
+      { name: "SQL", icon: "" },
+      { name: "PHP", icon: "" },
+      { name: "React", icon: "" },
+    ],
+    tools: [
+      { name: "Git & GitHub", icon: "" },
+      { name: "Vite", icon: "" },
+      { name: "Figma", icon: "" },
+    ],
+    projects: [
+      { title: "", teckStackTaks: "", link: "" },
+      { title: "", teckStackTaks: "", link: "" },
+      { title: "", teckStackTaks: "", link: "" },
+    ],
+    contact: {
+      email: "maryamarifgcc@gmail.com",
+      gitHubLink: "",
+      linkedInLink: "",
+      location: "Kabul, Afghanistan",
+    },
+  };
   return (
     // in here i well return JSX code
     <div>
