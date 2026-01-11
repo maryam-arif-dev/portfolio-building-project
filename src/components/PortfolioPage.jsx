1e391107a1cc1930a31c03b2710eb3933c564de0
Maryam Arif <bibi.maryam.arif@gmail.com>
2026-01-11 18:46:29 +0430

    feat: add initial header section structure
    Implement the main navigation and logo container within the PortfolioPage component. Includes links for About, Skills, Projects, and Contact with a mobile-ready layout structure.

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -1,3 +1,34 @@
 export default function PortfolioPage() {
-  return <div>{/* In here i well add all the components */}</div>;
+  return (
+    <dev className="general-body-container">
+      {/* Header Section */}
+      <header>
+        <div className="general-header-container">
+          <div className="logo-column">
+            <h1>Maryam Arif</h1>
+          </div>
+          <div class="navbar-column active">
+            <nav>
+              <ul class="nav-links">
+                <li>
+                  <a href="#" class="active-class-ul-li">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a href="">Skills and tools</a>
+                </li>
+                <li>
+                  <a href="#">Projects</a>
+                </li>
+                <li>
+                  <a href="#">Contact Me</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </dev>
+  );
 }
